cea4cc2304a2cbd748d900159a82dc81230f29d8
Aaryan Nagpal <aaryannagpal@gmail.com>
2024-11-30 20:45:35 +0530

    Create scheduler.cpp

---
+++ scheduler.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class Assignment {
+    // Define the Assignment class here
+};
+
+// Heap to store integers
+
+class Heap {
+    void print() {
+        for (int i = 0; i < heap.size(); ++i) {
+            std::cout << heap[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+
+class AssignmentScheduler {
+
+};
